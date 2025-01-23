491e4b55a5980db0b67c100871cf256f8d4d9640
Ester Oliveira <oliver.ester.2004@gmail.com>
2025-01-23 21:15:05 +0000

    dados api

--- python/server.py
+++ python/server.py
@@ -22,12 +22,12 @@ class WeatherService(WeatherServiceServicer):
         print(f"Recebida uma requisição para: {request.location}")
 
         # Dados simulados de previsão do tempo
-        response = WeatherResponse(
-            temperature=28.5,
-            humidity=75.0,
-            precipitation=0.0
+        response = requests.get(
+            'https://api.open-meteo.com/v1/forecast?latitude=-5.795&longitude=-35.2094&current=temperature_2m,apparent_temperature,precipitation&timezone=America%2FSao_Paulo'
         )
-        return response
+        weather_data = response.json()
+        temperature = weather_data["current"]["temperature_2m"]
+        return temperature
 
 #servidor
 async def server() -> None:
