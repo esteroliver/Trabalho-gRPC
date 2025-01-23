1b6a9d544e6d659a3c2464f7ddea8ce5e9c9a883
Ester Oliveira <oliver.ester.2004@gmail.com>
2025-01-23 20:08:55 +0000

    feat: função para fetch da api e para gerar o response

--- python/server.py
+++ python/server.py
@@ -1,5 +1,35 @@
 import grpc
+import asyncio
 import requests
+from grpc import aio
+from google.protobuf.json_format import MessageToJson
+from google.protobuf.struct_pb2 import Struct
+from concurrent import futures
 
-infos = requests.get('https://api.open-meteo.com/v1/forecast?latitude=-5.795&longitude=-35.2094&current=temperature_2m,apparent_temperature,precipitation&timezone=America%2FSao_Paulo')
-print(infos.json())
+#dados da api retornam sobre o tempo em Natal/RN
+def fetch_weather_data():
+    response = requests.get(
+        "https://api.open-meteo.com/v1/forecast?latitude=-5.795&longitude=-35.2094&current_weather=true&timezone=America%2FSao_Paulo"
+    )
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": "não foi possível acessar aos dados da API"}
+
+class WeatherService:
+    async def GetWeather(self, request, context):
+        weather_data = fetch_weather_data()
+        response_struct = Struct()
+        response_struct.update(weather_data)
+        return response_struct
+
+
+async def server() -> None:
+    server = grpc.aio.server()
+    server.add_insecure_port("[::]:50051")
+    print("Server running on port 50051...")
+    await server.start()
+    await server.wait_for_termination()
+
+if __name__== '__main__':
+    asyncio.run(server())
