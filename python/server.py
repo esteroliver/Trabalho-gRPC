2f09affe127cac60d5e6c8c4ab036ebfbc930bcd
Ester Oliveira <oliver.ester.2004@gmail.com>
2025-01-23 20:37:57 +0000

    feat: server finalizado

--- python/server.py
+++ python/server.py
@@ -6,7 +6,7 @@ from google.protobuf.json_format import MessageToJson
 from google.protobuf.struct_pb2 import Struct
 from concurrent import futures
 
-#dados da api retornam sobre o tempo em Natal/RN
+#fetch da API
 def fetch_weather_data():
     response = requests.get(
         "https://api.open-meteo.com/v1/forecast?latitude=-5.795&longitude=-35.2094&current_weather=true&timezone=America%2FSao_Paulo"
@@ -16,6 +16,7 @@ def fetch_weather_data():
     else:
         return {"error": "não foi possível acessar aos dados da API"}
 
+#serviço
 class WeatherService:
     async def GetWeather(self, request, context):
         weather_data = fetch_weather_data()
@@ -24,10 +25,23 @@ class WeatherService:
         return response_struct
 
 
+#servidor
 async def server() -> None:
     server = grpc.aio.server()
+    wheater_service = WeatherService()
+
+    #registrando o serviço
+    server.add_generic_rpc_handlers(
+        [
+            aio.unary_unary_rpc_method_handler(
+                weather_service.GetWeather,
+                request_deserializer=None,
+                response_serializer=MessageToJson,
+            )
+        ]
+    )
+    
     server.add_insecure_port("[::]:50051")
-    print("Server running on port 50051...")
     await server.start()
     await server.wait_for_termination()
 
